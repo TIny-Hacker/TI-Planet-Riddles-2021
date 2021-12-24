1f29c27d7509759b23db2269310d70b2a8280ed9
TIny_Hacker <newbbnews@gmail.com>
2021-12-24 07:48:28 -0500

    Update commented.py to day 24
    Now there are dots on the left side, along with the top.

--- commented.py
+++ commented.py
@@ -290,7 +290,8 @@ def qr_frame(v, x, y, c, z=1):
       if not dx or not dy:
         qr_mark(x + dx*z, y + dy*z, 7, c, z)
   for i in range(8, s-8, 2):                      #Begins to draw the QR Code in the box.
-    fill_rect(x + i*z, y + 6*z, z, z, c)
+    fill_rect(x + i*z, y + 6*z, z, z, c)          #Draws top dots
+    fill_rect(x + 6*z, y + i*z, z, z, c)          #Draws left dots
 
 qr_frame(qr_ver, x_qr + qr_margin, y_qr + qr_margin, (255,255,255), qr_zoom)      #Draws the tiny QR Code box in white
 show()
